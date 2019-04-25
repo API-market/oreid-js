387437490baadc19f7036e60ddeb5ec551963ec9
Steve Gehrman <sgehrman@gmail.com>
2019-04-25 12:27:24 -0700

    Added sourcemap to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,38 @@
-module.exports = {
-  output: {
-    library: 'eos-auth',
-    libraryTarget: 'umd'
-  },
-  target: 'web',
-  module: {
-    rules: [
-      {
-        enforce: 'pre',
-        test: /.(js)$/,
-        loader: 'eslint-loader',
-        exclude: /node_modules/,
-        options: {
-          fix: true
+// see https://github.com/webpack/webpack/issues/6460
+// wanted to get the mode
+module.exports = (env, argv) => {
+  let sourceMap = 'source-map'
+
+  if (argv.mode === 'development') {
+    sourceMap = 'inline-source-map'
+  }
+
+  return {
+    output: {
+      library: 'eos-auth',
+      libraryTarget: 'umd'
+    },
+    target: 'web',
+    devtool: sourceMap,
+    module: {
+      rules: [
+        {
+          enforce: 'pre',
+          test: /.(js)$/,
+          loader: 'eslint-loader',
+          exclude: /node_modules/,
+          options: {
+            fix: true
+          }
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
         }
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      }
-    ]
+      ]
+    }
   }
 }
