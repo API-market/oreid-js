11ed5f12c33041384150a743a08739d32ef38189
Tray Lewin <traylewin@Trays-iMac.local>
2020-05-31 13:25:33 -0700

    apply linter changes

--- src/localState.js
+++ src/localState.js
@@ -1,56 +1,55 @@
-import Helpers from './helpers';
-import StorageHandler from './storage';
+import Helpers from './helpers'
+import StorageHandler from './storage'
 
 // avoid Helpers.isNullOrEmpty, use isNullOrEmpty()
-const { isNullOrEmpty } = Helpers;
+const { isNullOrEmpty } = Helpers
 
 export default class LocalState {
   constructor(options) {
-    this.options = options;
-    this.cachedUser = null;
-    this.storage = new StorageHandler();
+    this.options = options
+    this.cachedUser = null
+    this.storage = new StorageHandler()
   }
 
   userKey() {
-    return `oreid.${this.options.appId}.user`;
+    return `oreid.${this.options.appId}.user`
   }
 
   saveUser(user) {
-    this.cachedUser = user;
+    this.cachedUser = user
 
     if (!isNullOrEmpty(this.cachedUser)) {
-      const serialized = JSON.stringify(this.cachedUser);
-      this.storage.setItem(this.userKey(), serialized);
+      const serialized = JSON.stringify(this.cachedUser)
+      this.storage.setItem(this.userKey(), serialized)
     }
   }
 
   accountName() {
     if (this.user()) {
-      return this.user().accountName;
+      return this.user().accountName
     }
 
-    return null;
+    return null
   }
-
 
   user() {
     if (!this.cachedUser) {
-      this.loadUser();
+      this.loadUser()
     }
 
-    return this.cachedUser;
+    return this.cachedUser
   }
 
   loadUser() {
-    this.cachedUser = null;
-    const serialized = this.storage.getItem(this.userKey());
+    this.cachedUser = null
+    const serialized = this.storage.getItem(this.userKey())
 
     if (!isNullOrEmpty(serialized)) {
-      this.cachedUser = JSON.parse(serialized);
+      this.cachedUser = JSON.parse(serialized)
     }
   }
 
   clear() {
-    this.storage.removeItem(this.userKey());
+    this.storage.removeItem(this.userKey())
   }
 }
