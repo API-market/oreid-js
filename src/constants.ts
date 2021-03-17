4d3d5e3401c667e674d6109febe26309d36fd69a
Tray Lewin <tray.lewin@live.com>
2021-03-17 09:40:04 -0700

    rebuild to republish npm package bump verion to 2.8.2 to be able to republish

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const providersNotImplemented = ['metro']
 export const defaultOreIdServiceUrl = 'https://service.oreid.io'
-export const version = '2.7.7' // Todo: This should come from package.json
+export const version = '2.8.2' // Todo: This should come from package.json
