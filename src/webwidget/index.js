46596339a4174a53db8740cbcddb6ac4ab9b2f43
Viktor Zavala <zavala.viktor@gmail.com>
2021-06-29 16:21:14 +0200

    [453] Fix url generation

--- src/webwidget/index.js
+++ src/webwidget/index.js
@@ -1,12 +1,13 @@
-import * as zoid from 'zoid/dist/zoid.frameworks'
 import { defaultOreIdServiceUrl } from '../constants'
 import DappActions from './DappActions'
 
 export const defineWebWidget = widgetName => {
   if (window) {
+    // eslint-disable-next-line global-require
+    const zoid = require('zoid/dist/zoid.frameworks')
     window[widgetName] = zoid.create({
       tag: 'oreid-web-widget',
-      url: `${process.env.OREID_URL || defaultOreIdServiceUrl}/webflow/v1`,
+      url: `${process.env.OREID_URL || defaultOreIdServiceUrl}/action/v1`,
       dimensions: {
         width: '500px',
         height: '500px',
