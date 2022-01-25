3e5c6cf4a6ccd968c79cad2f0a22a10243fcec71
Tray Lewin <tray.lewin@live.com>
2022-01-25 13:54:51 -0800

    hmac now uses crypto-js

--- src/hmac.ts
+++ src/hmac.ts
@@ -1,13 +1,12 @@
-import createHmac from 'create-hmac'
+import CryptoJS from 'crypto-js'
+// import createHmac from 'create-hmac'
 import Axios from 'axios'
 import Helpers from './helpers'
 
 export function generateHmac(secret: string, data: string) {
   // createHmac library works in browser or Nodejs
-  const hmac = createHmac('sha256', secret)
-  hmac.update(data)
-  const hash = hmac.digest('base64')
-  return hash
+  const hmac = CryptoJS.HmacSHA256(data, secret)
+  return hmac.toString()
 }
 
 /** Generate HMAC for url string and append it to end e.g. http:/nnnnnn&hmac=xxx */
