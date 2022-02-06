e2cae47c4a18af88159b97f1b9d3c053a51079a3
Tray Lewin <tray.lewin@live.com>
2022-02-06 15:10:10 -0800

    remove webwidget - its now downloaded from service endpoint

--- src/webwidget/index.js
+++
@@ -1,7 +0,0 @@
-import requireFromUrl from 'require-from-url/sync'
-
-const oreIdServiceBaseaUrl = process.env.ORE_ID_SERVICE_BASE_URL || 'https://service.oreid.io'
-
-const WebWidget = requireFromUrl(`${oreIdServiceBaseaUrl}/.well-known/ore-id-web-widget.js`)
-
-export default WebWidget
