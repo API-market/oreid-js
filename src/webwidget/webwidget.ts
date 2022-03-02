a08c179558b34b903f4507cb1d011051cf6bade2
artmanque <i@xn--rtman-ewa.com>
2022-03-02 12:43:53 +0500

    address change requests ENG-640

--- src/webwidget/webwidget.ts
+++ src/webwidget/webwidget.ts
@@ -1,71 +1,51 @@
-import { defaultHeight, defaultWidth, defaultOreIdServiceUrl } from './constants'
-import { WebWidgetAction, WebWidgetProps } from './models'
-import { webWidgetPropsSchema } from './helpers'
-
-export const version = '1'
-
-enum WebWidgetPropType {
-  Object = 'object',
-  String = 'string',
-  Function = 'function',
-}
-
-enum WindowType {
-  IFRAME = 'iframe',
-  POPUP = 'popup',
-}
+import { webWidgetDef } from './constants'
+import { getWindowTypeForProps, validateProps } from './helpers'
 
 /**
- * @returns var name created on window for widget
+ * @return var name created on window for widget
  * */
-export const createWebWidgetOnWindow = async (renderToWindow: Window = window, as = 'OreidWidget') => {
+export const createWebWidgetOnWindow = async (renderToWindow: Window = window) => {
+  const varOreidWidgetOnWindow = 'OreidWidget'
   if (typeof renderToWindow === 'undefined' || !renderToWindow) throw new Error('No window found to render widget')
   // eslint-disable-next-line global-require
   const zoid = require('zoid/dist/zoid')
-  // eslint-disable-next-line no-prototype-builtins
-  if (renderToWindow.hasOwnProperty(as)) await zoid.destroy()
-  // eslint-disable-next-line global-require
-  const OreidWidget = zoid.create({
-    tag: 'oreid-web-widget',
-    url: ({ props }: { props: WebWidgetProps }) =>
-      new URL('action', props?.oreIdOptions?.oreIdUrl || defaultOreIdServiceUrl).href,
-    dimensions: { width: defaultWidth, height: defaultHeight },
-    props: {
-      oreIdOptions: { type: WebWidgetPropType.Object },
-      action: { type: WebWidgetPropType.Object },
-      onSuccess: { type: WebWidgetPropType.Function },
-      onError: { type: WebWidgetPropType.Function },
-    },
-  })
-  if (!(await OreidWidget.canRenderTo(renderToWindow))) {
-    // eslint-disable-next-line no-param-reassign
-    delete renderToWindow[as as any]
-    throw new Error('Cannot render on window')
+  try {
+    let OreidWidget
+    // eslint-disable-next-line no-prototype-builtins
+    if (renderToWindow.hasOwnProperty(varOreidWidgetOnWindow)) {
+      OreidWidget = renderToWindow[varOreidWidgetOnWindow as any]
+    } else {
+      OreidWidget = zoid.create(webWidgetDef)
+      Object.defineProperties(renderToWindow, {
+        [varOreidWidgetOnWindow]: {
+          value: OreidWidget,
+          writable: true,
+        },
+      })
+    }
+    if (!(await OreidWidget.canRenderTo(renderToWindow))) {
+      // eslint-disable-next-line no-param-reassign
+      delete renderToWindow[varOreidWidgetOnWindow as any]
+      throw new Error('Cannot render on window')
+    }
+    return varOreidWidgetOnWindow
+  } catch (error) {
+    // eslint-disable-next-line no-prototype-builtins
+    if (renderToWindow.hasOwnProperty(varOreidWidgetOnWindow)) await zoid.destroy() // cleanup
+    throw error
   }
-  Object.defineProperties(renderToWindow, {
-    [as]: {
-      value: OreidWidget,
-      writable: true,
-    },
-  })
-  return as
 }
 
 /**
- * @returns rendered instance of widget
+ * @return rendered instance of widget
  * */
-export async function renderWebWidget(
-  webWidgetProps: WebWidgetProps,
-  container: string,
-  renderToWindow: Window = window,
-) {
+export async function renderWebWidget(webWidgetProps: unknown, container: string, renderToWindow: Window = window) {
   if (typeof renderToWindow === 'undefined' || !renderToWindow) throw new Error('No window found to render widget')
-  const props = await webWidgetPropsSchema.validate(webWidgetProps, { strict: true })
+  const props = validateProps(webWidgetProps)
   const containerElem = renderToWindow.document.querySelector(container)
   if (!containerElem) throw new Error(`No such element ${container} found on window's document`)
   const varOreidWidgetOnWindow = await createWebWidgetOnWindow(renderToWindow)
-  const context = props.action.name === WebWidgetAction.Auth ? WindowType.POPUP : WindowType.IFRAME
   const OreidWidgetComponent = (renderToWindow as any)[varOreidWidgetOnWindow](props)
-  OreidWidgetComponent.render(containerElem, context)
+  OreidWidgetComponent.render(containerElem, getWindowTypeForProps(props))
   return OreidWidgetComponent
 }
