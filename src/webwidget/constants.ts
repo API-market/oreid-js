75a776d959bc90aaaff9885cfc70c2f1f017696e
artmanque <i@xn--rtman-ewa.com>
2022-03-02 12:42:39 +0500

    webWidgetDef is a constant

--- src/webwidget/constants.ts
+++ src/webwidget/constants.ts
@@ -1,4 +1,18 @@
+import { PROP_TYPE } from 'zoid/dist/zoid'
+import { WebWidgetProps } from './models'
+import { defaultOreIdServiceUrl } from '../constants'
+
 export const version = '1.0.0' // Todo: This should come from package.json
-export const defaultHeight = '740px'
-export const defaultWidth = '420px'
-export { defaultOreIdServiceUrl } from '../constants'
+export { defaultOreIdServiceUrl }
+export const webWidgetDef = {
+  tag: 'oreid-web-widget',
+  url: ({ props }: { props: WebWidgetProps }) =>
+    new URL('action', props?.oreIdOptions?.oreIdUrl || defaultOreIdServiceUrl).href,
+  dimensions: { width: '420px', height: '740px' },
+  props: {
+    oreIdOptions: { type: PROP_TYPE.OBJECT },
+    action: { type: PROP_TYPE.OBJECT },
+    onSuccess: { type: PROP_TYPE.FUNCTION },
+    onError: { type: PROP_TYPE.FUNCTION },
+  },
+}
