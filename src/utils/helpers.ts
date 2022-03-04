5185c7b6034476b0e49c0b8505bd4b6d51687c30
Tray Lewin <tray.lewin@live.com>
2022-03-04 15:12:38 -0800

    add isValidEmail() helper

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -339,4 +339,9 @@ export default class Helpers {
     return provider === AuthProvider.Custodial
   }
 
+  static isValidEmail(email: any): boolean {
+    if (!email) return false
+    const emailRegex = /^(([^<>()[]\\.,;:\s@]+(\.[^<>()[]\\.,;:\s@]+)*)|(.+))@(([[0-9]{1,3}\[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return emailRegex.test(email)
+  }
 }
