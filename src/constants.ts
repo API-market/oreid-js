a5dbba10c859a4dcba50f2ef54ff2b37cca5310e
Tray Lewin <tray.lewin@live.com>
2022-03-21 19:15:51 -0700

    use correct defaultOreIdServiceUrl

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import { ApiEndpoint } from './models'
 
 export const providersNotImplemented = ['metro']
-export const defaultOreIdServiceUrl = 'http://localhost:8080' // 'https://service.oreid.io'
-export const version = '3.0.0' // Todo: This should come from package.json
+export const defaultOreIdServiceUrl = 'https://service.oreid.io'
+export const version = '3.0.7' // Todo: This should come from package.json
 export const publicApiEndpoints = [ApiEndpoint.LoginUserWithToken] // api endpoints that dont require authorization
