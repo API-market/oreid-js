fb14689553cd19ef06a9299041332d2efbc7bb1d
Buddy Deck <38056253+deckb@users.noreply.github.com>
2022-03-30 16:13:11 -0400

    pull version from package.json

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,12 @@
 import { ApiEndpoint } from './models'
+const pjson = require('../package.json');
 
 export const providersNotImplemented = ['metro']
 export const defaultOreIdServiceUrl = 'https://service.oreid.io'
-export const version = '3.0.7' // Todo: This should come from package.json
+
+let tag_version = pjson.version
+if(process.env.CIRCLE_TAG){
+    tag_version = process.env.CIRCLE_TAG
+}
+export const version = tag_version
 export const publicApiEndpoints = [ApiEndpoint.LoginUserWithToken] // api endpoints that dont require authorization
