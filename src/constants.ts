2a27f35c95f6ce82d402fc28705b532524ad74a7
Buddy Deck <38056253+deckb@users.noreply.github.com>
2022-03-30 17:29:05 -0400

    use circleci to set version based on package.json

--- src/constants.ts
+++ src/constants.ts
@@ -1,12 +1,7 @@
 import { ApiEndpoint } from './models'
-const pjson = require('../package.json');
 
 export const providersNotImplemented = ['metro']
 export const defaultOreIdServiceUrl = 'https://service.oreid.io'
-
-let tag_version = pjson.version
-if(process.env.CIRCLE_TAG){
-    tag_version = process.env.CIRCLE_TAG
-}
-export const version = tag_version
+// This is updated at build time if using circleci
+export const version = '0.0.1'
 export const publicApiEndpoints = [ApiEndpoint.LoginUserWithToken] // api endpoints that dont require authorization
