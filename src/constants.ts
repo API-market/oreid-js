31c9c5f6b0cb3022578a9102bc60430bdba414d5
Buddy Deck <38056253+deckb@users.noreply.github.com>
2022-03-31 09:40:57 -0400

    revert to using package.json version

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,8 @@
 import { ApiEndpoint } from './models'
+const pjson = require('../package.json');
 
 export const providersNotImplemented = ['metro']
 export const defaultOreIdServiceUrl = 'https://service.oreid.io'
-// This is updated at build time if using circleci
-export const version = '0.0.1'
+
+export const version = pjson.version
 export const publicApiEndpoints = [ApiEndpoint.LoginUserWithToken] // api endpoints that dont require authorization
