0939848d2db3fe3fbdc73fa642e5e21302d8d793
Buddy Deck <38056253+deckb@users.noreply.github.com>
2022-03-31 09:37:51 -0400

    revert inadvertent changes to master

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,6 @@
 import { ApiEndpoint } from './models'
-const pjson = require('../package.json');
 
 export const providersNotImplemented = ['metro']
 export const defaultOreIdServiceUrl = 'https://service.oreid.io'
-
-export const version = pjson.version
+export const version = '3.0.7' // Todo: This should come from package.json
 export const publicApiEndpoints = [ApiEndpoint.LoginUserWithToken] // api endpoints that dont require authorization
