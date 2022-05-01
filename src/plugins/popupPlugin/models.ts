822bb84aa4165c4091f598cb90269c3284cb8934
Tray Lewin <tray.lewin@live.com>
2022-04-30 18:26:25 -0700

    popup return types now all return objects

--- src/plugins/popupPlugin/models.ts
+++ src/plugins/popupPlugin/models.ts
@@ -1,19 +1,24 @@
-import { ChainNetwork } from '../../common/models'
 import Transaction from '../../transaction/transaction'
 import { UserData } from '../../user/models'
 import {
   WebWidgetAuthParams,
+  WebWidgetAuthResult,
   WebWidgetLogoutResult,
+  WebWidgetNewChainAccountParams,
+  WebWidgetNewChainAccountResult,
+  WebWidgetRecoverAccountParams,
   WebWidgetRecoverAccountResult,
   WebWidgetSignResult,
 } from '../../webWidget/models'
 
 export type PopupPluginError = ({ errors, data }: { errors?: string; data?: any }) => void
 
+export type PopupPluginAuthSuccessResults = WebWidgetAuthResult & { user: UserData }
+
 export interface PopupPluginAuthParams {
   params: WebWidgetAuthParams
   onError?: PopupPluginError
-  onSuccess?: (user: UserData) => void
+  onSuccess?: (result: PopupPluginAuthSuccessResults) => void
 }
 
 export interface PopupPluginSignParams {
@@ -22,14 +27,16 @@ export interface PopupPluginSignParams {
   onSuccess?: (result: WebWidgetSignResult) => void
 }
 
+export type PopupPluginNewChainAccountSuccessResults = WebWidgetNewChainAccountResult & { chainNetwork: string }
+
 export interface PopupPluginNewChainAccountParams {
-  params: { accountType?: string; chainNetwork: ChainNetwork; accountOptions?: any }
+  params: Partial<WebWidgetNewChainAccountParams>
   onError?: PopupPluginError
-  onSuccess?: (chainAccount: string) => void
+  onSuccess?: (result: PopupPluginNewChainAccountSuccessResults) => void
 }
 
 export interface PopupPluginRecoverAccountParams {
-  params: { provider?: string; recoverAction?: string }
+  params: Partial<WebWidgetRecoverAccountParams>
   onError?: PopupPluginError
   onSuccess?: (result: WebWidgetRecoverAccountResult) => void
 }
