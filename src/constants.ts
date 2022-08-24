133c3c1aff2499fd1b30a83b4722db46095cfbbc
Bruno Motta <wadge.motta@gmail.com>
2022-08-24 11:01:42 -0300

    Removing comments

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,7 @@
 import { ApiEndpoint } from './models'
 export { version } from './version.json'
-// const pjson = require('../package.json')
-// export const { version } = pjson
 
 export const providersNotImplemented = ['metro']
 export const defaultOreIdServiceUrl = 'https://service.oreid.io'
 
-// export const version = '4.2.99'
 export const publicApiEndpoints = [ApiEndpoint.LoginUserWithToken, ApiEndpoint.NewUserWithToken] // api endpoints that dont require authorization
