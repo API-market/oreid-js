67c94bff8adce7d650757b7a3c7f4c46698c4dbe
carl <pixxiec@hotmail.com>
2022-12-19 20:45:41 +0200

    CircleCI failures

--- src/transaction/models.spec.ts
+++ src/transaction/models.spec.ts
@@ -1,15 +1,49 @@
-import { TransactionData } from './models'
+import { ChainNetwork } from '../models'
+import { TransactionData, TransactionSignOptions } from './models'
 
 function isTransactionData(obj: any): obj is TransactionData {
-  return 'accessToken' in obj && obj.accessToken === 'accessToken value'
+  console.log(`signOptions: ${JSON.stringify(typeof obj.signOptions)}`)
+  return (
+    'account' in obj &&
+    obj.account === 'account value' &&
+    'chainAccount' in obj &&
+    obj.chainAccount === 'chainAccount value' &&
+    'chainNetwork' in obj &&
+    obj.chainNetwork === 'algo_beta' &&
+    'expireSeconds' in obj &&
+    obj.expireSeconds === 5 &&
+    'signedTransaction' in obj &&
+    'stx' in obj.signedTransaction &&
+    obj.signedTransaction.stx === 'stx value' &&
+    'transaction' in obj &&
+    'tx' in obj.transaction &&
+    obj.transaction.tx === 'tx value' &&
+    'encodedSignedTransaction' in obj &&
+    obj.encodedSignedTransaction === 'encodedSignedTransaction value' &&
+    'encodedTransaction' in obj &&
+    obj.encodedTransaction === 'encodedTransaction value' &&
+    'transactionChainAccount' in obj &&
+    obj.transactionChainAccount === 'transactionChainAccount value' &&
+    'transactionRecordId' in obj &&
+    obj.transactionRecordId === 'transactionRecordId value' &&
+    'signOptions' in obj
+  )
 }
 
+const tso: TransactionSignOptions = {}
+
 const td: TransactionData = {
-  accessToken: 'accessToken value',
-  appId: 'appId value',
-  backgroundColor: 'backgroundColor value',
-  oreIdUrl: 'oreIdUrl value',
-  eosTransitWalletProviders: ['eosTransitWalletProviders value'],
+  account: 'account value',
+  chainAccount: 'chainAccount value',
+  chainNetwork: ChainNetwork.AlgoBeta,
+  expireSeconds: 5,
+  signedTransaction: { stx: 'stx value' },
+  transaction: { tx: 'tx value' },
+  encodedSignedTransaction: 'encodedSignedTransaction value',
+  encodedTransaction: 'encodedTransaction value',
+  transactionChainAccount: 'transactionChainAccount value',
+  transactionRecordId: 'transactionRecordId value',
+  signOptions: tso,
 }
 
 describe('Transaction Data type', () => {
